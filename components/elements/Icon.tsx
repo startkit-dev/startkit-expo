25ff9fca01b925d78cfc21d0cf917b7aca793de2
Matt Venables <matt@venabl.es>
2023-04-09 19:38:53 -0400

    Use Lucide icons instead of Feather

--- components/elements/Icon.tsx
+++ components/elements/Icon.tsx
@@ -1,20 +1,33 @@
-import { Feather } from "@expo/vector-icons"
 import { clsx } from "clsx"
-
-import type { ComponentProps } from "react"
-
-export type IconProps = ComponentProps<typeof Feather>
+import { type LucideProps, X } from "lucide-react-native"
 
 /**
- * A dark-mode aware wrapper around the <Feather> vector icon component which
+ * List all icons used in the app here.
+ *
+ * This is used to limit the bundle size by only importing the icons we use.
+ */
+export const icons = {
+  x: X
+}
+
+export type IconProps = LucideProps & {
+  name: keyof typeof icons
+}
+
+/**
+ * A dark-mode aware wrapper around the Lucide vector icon component which
  * applies our basic styles.
  *
  * If a color is provided, it will be used. Otherwise, the default color will be
  * used.
+ *
+ * For icons, see: https://lucide.dev
  */
-export function Icon({ color, ...props }: IconProps) {
+export function Icon({ name, color, ...props }: IconProps) {
+  const LucideIcon = icons[name]
+
   return (
-    <Feather
+    <LucideIcon
       className={clsx(!color && "text-black dark:text-white")}
       color={color}
       {...props}
